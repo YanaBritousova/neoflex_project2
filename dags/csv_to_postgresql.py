3b00ae40fcb2a9973915571bafd30dd1b259d902
unknown <yarbritousova@gmail.com>
2026-05-19 12:59:01 +0400

    change load strategy for product

--- dags/csv_to_postgresql.py
+++ dags/csv_to_postgresql.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 def start_func():
     logger.info("DAG started")
 
-def load_csv_to_postgres(file_name, table_name, primary_key, **context):
+def add_csv_to_postgres(file_name, table_name, primary_key, **context):
     csv_path = f"/opt/airflow/data/{file_name}"
     
     pg_hook = PostgresHook(postgres_conn_id='postgres_default')
@@ -76,6 +76,52 @@ def load_csv_to_postgres(file_name, table_name, primary_key, **context):
         logger.error(f"Ошибка при загрузке {csv_path}: {str(e)}")
         raise
 
+def reload_csv_to_postgres(file_name, table_name, **context):
+    csv_path = f"/opt/airflow/data/{file_name}"
+    
+    pg_hook = PostgresHook(postgres_conn_id='postgres_default')
+    
+    try:
+        if not os.path.exists(csv_path):
+            raise FileNotFoundError(f"Файл не найден: {csv_path}")
+        
+
+        df = None
+        for enc in ['cp1251', 'windows-1251', 'latin1', 'utf-8']:
+            try:
+                df = pd.read_csv(csv_path, sep=',', encoding=enc)
+                logger.info(f"Файл {csv_path} прочитан с кодировкой {enc}")
+                break
+            except UnicodeDecodeError:
+                continue
+        else:
+            raise UnicodeDecodeError(f"Не удалось прочитать файл {csv_path} ни в одной из кодировок")
+        
+        logger.info(f"Файл {csv_path} прочитан")
+                
+        target_columns = list(df.columns)
+        logger.info(f"Найдены колонки в файле: {target_columns}")
+        logger.info(f"Прочитано {len(df)} строк из {csv_path}")
+
+        records = [tuple(None if pd.isna(val) else val for val in row) 
+                   for row in df[target_columns].itertuples(index=False)]
+
+    
+        trunc_sql =f"""truncate table {table_name} cascade"""
+        pg_hook.run(trunc_sql)
+    
+        pg_hook.insert_rows(
+            table=table_name,
+            rows=records,
+            target_fields=target_columns,
+            replace=False
+        )
+        
+            
+    except Exception as e:
+        logger.error(f"Ошибка при загрузке {csv_path}: {str(e)}")
+        raise
+
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2024, 1, 1),
@@ -96,14 +142,14 @@ with DAG(
     
     load_deal = PythonOperator(
         task_id="load_deal_to_postgres",
-        python_callable=load_csv_to_postgres,
+        python_callable=add_csv_to_postgres,
         op_kwargs={"file_name": "deal_info.csv", "table_name": "rd.deal_info", "primary_key": ["deal_rk", "effective_from_date"]}
     )
     
     load_product = PythonOperator(
         task_id="load_product_to_postgres",
-        python_callable=load_csv_to_postgres,
-        op_kwargs={"file_name": "product_info.csv", "table_name": "rd.product", "primary_key": ["product_rk", "effective_from_date"]}
+        python_callable=reload_csv_to_postgres,
+        op_kwargs={"file_name": "product_info.csv", "table_name": "rd.product"}
     )
 
     start >> load_deal >> load_product
